cb6958474a611590ab0df99dbfd35fe5e6cea70d
Neil Shweky <neilshweky@lftlstats.herokuapp.com>
2018-03-26 01:34:43 -0400

    Updated README and added support for replying to comments.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 Hey! This is my "Hello, World" app for Probot's Google Summer of Code application. I tried to trick it out a little to prove my knowledge of the APIs. I hope you like it!
 
+I added functionality for the bot responding to comments,but only comments not made my bots... (I got stuck in one infinite loop too many). 
+
+My ultimate goal for this "Hello, World" project is to be able to compile all of the open issue titles into one central locked issue.
+
 ## Setup
 
 ```sh
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 module.exports = robot => {
-  robot.on('issues.opened', async context => {
+  robot.on(['issues.opened','issue_comment.created'], async context => {
     
     
-    context.log("Hello \n" + context)
+    // context.log("Hello \n" + JSON.stringify(context, null, 4));
     const params = context.issue({body: 'Hello World!'})
 
-    
-    return context.github.issues.createComment(params)
-  })
+    if(!context.isBot){
+      return context.github.issues.createComment(params);
+    }else{
+      return;
+    }
+
+
+  });
 }
