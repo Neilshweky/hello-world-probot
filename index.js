d0f7e5173726e785694fcf17449a5e5e4bf05ddb
Neil Shweky <neilshweky@lftlstats.herokuapp.com>
2018-03-27 09:38:57 -0400

    testing

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ module.exports = robot => {
     
     // context.log("Hello \n" + JSON.stringify(context, null, 4));
     const params = context.issue({body: 'Hello World!'})
-
+    context.log('hello');
     context.github.getForRepo({
       owner: context.payload.repository.owner.id, 
       repo: context.payload.repository.id
-    }, (error, result) => {})
+    }, (error, result) => {
+      if(error) context.log(error)
+      context.log(JSON.stringify(result, null, 4))
+    })
 
     if(!context.isBot){
       return context.github.issues.createComment(params);
