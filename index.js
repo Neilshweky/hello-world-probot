d2531ed020b50901a1de287a6072c652fccc35ef
Neil Shweky <neilshweky@lftlstats.herokuapp.com>
2018-03-27 11:14:59 -0400

    Now supports getting and logging all of the issue names Also supports bot-created comments.

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ module.exports = robot => {
   robot.on(['issues.opened','issue_comment.created'], async context => {
     
     
-    // context.log("Hello \n" + JSON.stringify(context, null, 4));
-    const params = context.issue({body: 'Hello World!'})
-    context.log('hello');
-    context.github.getForRepo({
-      owner: context.payload.repository.owner.id, 
-      repo: context.payload.repository.id
-    }, (error, result) => {
-      if(error) context.log(error)
-      context.log(JSON.stringify(result, null, 4))
-    })
 
     if(!context.isBot){
+
+      
+      
+
+      const params = context.issue({body: 'Hello World!'})
       return context.github.issues.createComment(params);
     }else{
       return;
@@ -21,4 +16,37 @@ module.exports = robot => {
 
 
   });
+
+  
+
+  //Gets and logs all issue names, not needed just wanted to test it.
+  function logIssueNames(context) {
+
+    console.log('owner: ' + context.payload.repository.owner.login);
+    console.log('repo: ' + context.payload.repository.name);
+
+    context.github.issues.getForRepo({
+      owner: context.payload.repository.owner.login, 
+      repo: context.payload.repository.name
+    }, (error, result) => {
+      if(error) console.log('Error' + error)
+      else{
+        console.log('Result' + JSON.stringify(result, null, 4))
+        result.data.forEach(element => {
+          console.log('#' + element.number + ' ' + element.title + '\n\n' + element.body + '\n');
+        });
+      }
+    })
+
+  }
+
+  function createBotIssue (context){
+    
+    context.github.issues.create({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      title: 'BOT: Issues Summary: Welcome Newcomers!',
+      body: 'Hey everyone: stuff would go here.....'
+    });
+  }
 }
